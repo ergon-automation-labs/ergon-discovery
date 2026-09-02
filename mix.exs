473d755d0a80783023be6df6935b600db773088a
Abby Malson <august.malson@gmail.com>
2026-09-02 08:52:05 +0300

    chore: Bump version to 0.1.6 to pick up runtime v0.14.62

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyDiscovery.MixProject do
   def project do
     [
       app: :bot_army_discovery,
-      version: "0.1.0",
+      version: "0.1.6",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -26,15 +26,17 @@ defmodule BotArmyDiscovery.MixProject do
   defp deps do
     [
       {:bot_army_library_core, path: "../../elixir_bots/bot_army_library_core", override: true},
-      {:bot_army_library_runtime, path: "../../elixir_bots/bot_army_library_runtime", override: true},
-      {:bot_army_library_learning, path: "../../elixir_bots/bot_army_library_learning", override: true},
+      {:bot_army_library_runtime,
+       path: "../../elixir_bots/bot_army_library_runtime", override: true},
+      {:bot_army_library_learning,
+       path: "../../elixir_bots/bot_army_library_learning", override: true},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, "~> 0.17"},
       {:jason, "~> 1.4"},
       {:logger_json, "~> 5.1"},
       {:elixir_uuid, "~> 1.2"},
       {:req, "~> 0.3"},
-
+      {:toml, "~> 0.6"},
       # Development/Test
       {:ex_doc, "~> 0.30", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
